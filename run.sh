429744aedfac47cd0966a401e6f1af05f834774e
Knut Ahlers <knut@ahlers.me>
2016-07-29 00:51:20 +0200

    parameterize naming, improve PDF generation

--- run.sh
+++ run.sh
@@ -4,15 +4,12 @@ DPI=150
 W=1754 # 150 x 11.69
 H=1241 # 150 x  8.27
 
-/usr/bin/phantomjs /src/index.js "$@"
+/usr/bin/phantomjs /src/index.js $1 $2 $3
 
-TARGET="map_$(date +%Y-%m-%d_%H-%M)"
+TARGET="$4_$(date +%Y-%m-%d_%H-%M)"
 mv /data/shot.png /data/${TARGET}.png
 
-#exec convert /data/${TARGET}.png -compress jpeg -quality 90 \
-#  -resize ${W}x${H} -density ${DPI} \
-#  -repage ${W}x${H} /data/${TARGET}.pdf
 exec convert /data/${TARGET}.png -compress jpeg -quality 90 \
   -resize ${W}x${H} -gravity center -extent ${W}x${H} \
-  -units PixelsPerInch -set density ${DPI} \
+  -units PixelsPerInch -density ${DPI} \
   -repage ${W}x${H} /data/${TARGET}.pdf
